b66eb64b031c4562c19b7939303d5ea12be5c560
ponychan <rzsckbmx@163.com>
2017-10-06 11:25:34 +0800

    2.8.cpp
    用循环求一个正整数的所有因子

--- ConsoleApplication2_8/ConsoleApplication28.cpp
+++ ConsoleApplication2_8/ConsoleApplication28.cpp
@@ -1,11 +1,21 @@
 // ConsoleApplication28.cpp: 定义控制台应用程序的入口点。
-//
+//2.8.cpp求一个正整数的所有因子
 
 #include "stdafx.h"
-
+#include<iostream>
+using namespace std;
 
 int main()
 {
-    return 0;
+	int n;
+	cout << "input a positive integer";
+	cin >> n;
+	cout << "number " << n << " factors";
+	for (int k = 1; k <= n; k++) {
+		if (n%k == 0)
+			cout << k << " ";
+	}
+	cout << endl;
+	return 0;
 }
 
